13fa4c22bb9818c1e68bc71eede192304f0137b0
NouhailaSif <soussif.nouhaila@gmail.com>
2023-10-18 16:30:02 -0700

    3rd commit

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- database dump
+
+SELECT tv_genres.name AS genre,
+       COUNT(tv_show_genres.genre_id) AS number_shows
+       FROM tv_genres INNER JOIN tv_show_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+       GROUP BY tv_genres.name
+       ORDER BY number_shows DESC;
