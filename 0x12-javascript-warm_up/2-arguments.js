6e92aaae6ecc31f75d6789ca1e70cb287318d452
NouhailaSif <soussif.nouhaila@gmail.com>
2023-11-13 12:36:54 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-// print a message depending on the arg
 
-const arvlength = process.argv.length
-
-if (argvlength === 2) {
-	console.log('No argument');
-} else if (argvlength === 3) {
-	console.log('Argument found');
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
